cdd3acfc6b8c64ddacaa4e1660f7afa960a79e9f
hujinsen <hujsen@163.com>
2025-10-02 10:12:25 +0800

    Add demo4.py and text_format.py

---
+++ text_format.py
@@ -0,0 +1,28 @@
+import json
+import os
+from dashscope import Generation
+import dashscope 
+
+# 若使用新加坡地域的模型，请释放下列注释
+# dashscope.base_http_api_url = "https://dashscope-intl.aliyuncs.com/api/v1"
+messages = [
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "你是谁？"},
+]
+response = Generation.call(
+    # 若没有配置环境变量，请用阿里云百炼API Key将下行替换为：api_key = "sk-xxx",
+    # 新加坡和北京地域的API Key不同。获取API Key：https://help.aliyun.com/zh/model-studio/get-api-key
+    # api_key=os.getenv("DASHSCOPE_API_KEY"),
+    api_key = "sk-2d627fbbc4fa491db207c632a77f2852",
+    model="qwen-plus",
+    messages=messages,
+    result_format="message",
+)
+
+if response.status_code == 200:
+    print(json.dumps(response, default=lambda o: o.__dict__, indent=4,ensure_ascii=False))
+else:
+    print(f"HTTP返回码：{response.status_code}")
+    print(f"错误码：{response.code}")
+    print(f"错误信息：{response.message}")
+    print("请参考文档：https://help.aliyun.com/zh/model-studio/developer-reference/error-code")
